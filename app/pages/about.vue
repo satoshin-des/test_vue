3af3f42f7451d20403e19b5a79bce95806ad1ce8
Nagato <transcendentalliouville@gmail.com>
2026-04-23 11:44:45 +0900

    test publication

---
+++ app/pages/about.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>HeyHeyHey</h1>
+    </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>ホームページです</h1>
+        <NuxtLink to="/about">アバウト</NuxtLink>
+    </div>
+</template>
